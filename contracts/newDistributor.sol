8945e748208ee2800aa431b7d8371625b3ea9966
rafa <rafa.canseco@gmail.com>
2024-01-22 14:48:31 -0600

    first commit

---
+++ contracts/newDistributor.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract newDistributor is Ownable {
+    //State variables and mappings
+    mapping(address => bool) public isHolder;
+    mapping(address => uint256) public holderPercentages;
+    address[] public holders;
+    IERC20 public tokenToTrack;
+    IERC20 public airdropToken;
+
+    //Events
+    event TokenToTrackRegistered(IERC20 indexed token);
+    event AirdropTokenRegistered(IERC20 indexed token);
+    event AidropExecuted(address indexed holder, uint256 amount);
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
+    function registerTokenToTrack(IERC20 _tokenToTrack) public onlyOwner {
+        tokenToTrack = _tokenToTrack;
+        emit TokenToTrackRegistered(_tokenToTrack);
+    }
+
+    function registerAirdropToken(IERC20 _airdropToken) public onlyOwner {
+        airdropToken = _airdropToken;
+        emit AirdropTokenRegistered(_airdropToken);
+    }
+
+    function registerHolders(address[] calldata _holders) external onlyOwner {
+        for (uint i = 0; i < _holders.length; i++) {
+            if (!isHolder[_holders[i]]) {
+                isHolder[_holders[i]] = true;
+                holders.push(_holders[i]);
+                uint256 balance = tokenToTrack.balanceOf(_holders[i]);
+                uint256 totalSupply = tokenToTrack.totalSupply();
+                holderPercentages[_holders[i]] = (balance * 100) / totalSupply;
+            }
+        }
+    }
+
+    function airdrop() public onlyOwner {
+        uint256 totalAirdropAmount = airdropToken.balanceOf(address(this));
+        require(
+            totalAirdropAmount > 0,
+            "there is no tokens of airdrop tokens on the contract"
+        );
+
+        for (uint256 i = 0; i < holders.length; i++) {
+            address holder = holders[i];
+            uint256 holderPercentage = holderPercentages[holder];
+            uint256 holderAmount = (totalAirdropAmount * holderPercentage) /
+                100;
+            require(
+                airdropToken.balanceOf(address(this)) >= holderAmount,
+                "Insufficient tokens for airdrop"
+            );
+            airdropToken.transfer(holder, holderAmount);
+            emit AidropExecuted(holder, holderAmount);
+        }
+        resetHolders();
+    }
+
+    function withdrawToken(
+        IERC20 _token,
+        address _to,
+        uint256 _amount
+    ) public onlyOwner {
+        require(
+            _token.balanceOf(address(this)) >= _amount,
+            "Insufficient token balance in contract"
+        );
+        bool success = _token.transfer(_to, _amount);
+        require(success, "Token transfer failed");
+    }
+
+    function emergencyWithdrawAvax() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No AVAX balance to withdraw");
+        (bool success, ) = payable(owner()).call{value: balance}("");
+        require(success, "Transfer failed");
+    }
+
+    function resetHolders() public onlyOwner {
+        for (uint i = 0; i < holders.length; i++) {
+            address holder = holders[i];
+            isHolder[holder] = false;
+            holderPercentages[holder] = 0;
+        }
+        delete holders;
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,76 @@
+import { HardhatUserConfig,task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const FORK_FUJI = false
+const FORK_MAINNET = true
+
+const forkingData = FORK_FUJI
+  ? {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+    }
+  : FORK_MAINNET
+  ? {
+      url: process.env.RPC_ENDPOINT_MAINNET || "",
+      blockNumber: 
+      39682887,
+    }
+  : undefined
+
+  const config: HardhatUserConfig = {
+    solidity: {
+      compilers: [
+        {
+          version: "0.7.4",
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 200,
+            },
+          },
+        },
+        {
+          version: "0.8.20",
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 200,
+            },
+          },
+        },
+      ],
+    },
+    etherscan: {
+      apiKey: {
+        avalanche: "snowtrace", 
+      }
+    },
+    networks: {
+      hardhat: {
+        forking: forkingData
+      },
+      fuji: {
+        url: process.env.RPC_ENDPOINT_TESTNET || "",
+        gasPrice: 225000000000,
+        chainId: 43113,
+        accounts:
+          process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      },
+      mainnet:{
+        url: process.env.RPC_ENDPOINT_MAINNET || "",
+        gasPrice: 225000000000,
+        chainId: 43114,
+        accounts: 
+          process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      }
+    }
+  };
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+     console.log(account.address);
+  }
+});
+
+export default config;
