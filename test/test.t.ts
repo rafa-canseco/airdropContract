06707508ebbc18b80f5b65e9a1366b23957045dd
rafa <rafa.canseco@gmail.com>
2024-01-23 13:20:52 -0600

    testing completo

--- test/test.t.ts
+++ test/test.t.ts
@@ -127,8 +127,118 @@ describe("newDistributor", function () {
             expect(balanceFinalContrato).to.equal(0);
             expect(balanceFinalOwner).to.be.above(balanceInicialOwner);
         })
+        it("should do the airdrop",async function () {
+            //enviar tokens al contrato
+            const distributorAddress = await distributor.getAddress()
+            const tokenAddress= "0x4F94b8AEF08c92fEfe416af073F1Df1E284438EC"
+            const tokenToTrack = "0x1785d6F5B076d93EfAE079744D0c86F2aC77621f"
+            const tokenContract = await ethers.getContractAt("IERC20", tokenAddress);
+            addressImpersonator = "0x91e4e6a5eb7823D9933F42B9c67583bb6f622d65";
+            await network.provider.request({
+                method: "hardhat_impersonateAccount",
+                params: [addressImpersonator]
+            });
+            const amount = ethers.parseUnits("1000006910",18)
+            const signer = await ethers.provider.getSigner(addressImpersonator)
+            await tokenContract.connect(signer).transfer(distributorAddress,amount)
+            //registrar el token to track
+            distributor.registerTokenToTrack(tokenToTrack)
+            //registrar el token airdrop
+            await distributor.registerAirdropToken(tokenAddress)
+            //insertar los addressess
+            const address1 = "0xB699684651fF748A6357892e006A0BDAa1707D1a";
+            const address2 = "0xD5AFAcFE7061042695e67210E2C347163683e30c";
+            const address3 = "0x29fB012F31128eD9C4f4C7a8a7CE01bF6Fc641C4";
+            const direcciones = [address1, address2, address3];
+            await distributor.registerHolders(direcciones);
+            //balances airdrop antes
+            const balance1Before = await tokenContract.balanceOf(address1)
+            const balance2Before = await tokenContract.balanceOf(address2)
+            const balance3Before = await tokenContract.balanceOf(address3)
+            //llamar airdrop
+            await distributor.airdrop()
+            const balance1after = await tokenContract.balanceOf(address1)
+            const balance2after = await tokenContract.balanceOf(address2)
+            const balance3after = await tokenContract.balanceOf(address3)
+            //expect
+            expect(balance1after).to.be.above(balance1Before);
+            expect(balance2after).to.be.above(balance2Before);
+            expect(balance3after).to.be.above(balance3Before);
+        })
+        it("should airdrop, change airdrop token and repeat airdrop",async function () {
+            //primer ciclo
+            //enviar tokens al contrato
+            const distributorAddress = await distributor.getAddress()
+            const tokenAddress= "0x4F94b8AEF08c92fEfe416af073F1Df1E284438EC"
+            const tokenToTrack = "0x1785d6F5B076d93EfAE079744D0c86F2aC77621f"
+            const tokenContract = await ethers.getContractAt("IERC20", tokenAddress);
+            addressImpersonator = "0x91e4e6a5eb7823D9933F42B9c67583bb6f622d65";
+            await network.provider.request({
+                method: "hardhat_impersonateAccount",
+                params: [addressImpersonator]
+            });
+            const amount = ethers.parseUnits("1000006910",18)
+            const signer = await ethers.provider.getSigner(addressImpersonator)
+            await tokenContract.connect(signer).transfer(distributorAddress,amount)
+            //registrar el token to track
+            distributor.registerTokenToTrack(tokenToTrack)
+            //registrar el token airdrop
+            await distributor.registerAirdropToken(tokenAddress)
+            //insertar los addressess
+            const address1 = "0xB699684651fF748A6357892e006A0BDAa1707D1a";
+            const address2 = "0xD5AFAcFE7061042695e67210E2C347163683e30c";
+            const address3 = "0x29fB012F31128eD9C4f4C7a8a7CE01bF6Fc641C4";
+            const direcciones = [address1, address2, address3];
+            await distributor.registerHolders(direcciones);
+            //balances airdrop antes
+            const balance1Before = await tokenContract.balanceOf(address1)
+            const balance2Before = await tokenContract.balanceOf(address2)
+            const balance3Before = await tokenContract.balanceOf(address3)
+            //llamar airdrop
+            await distributor.airdrop()
+            const balance1after = await tokenContract.balanceOf(address1)
+            const balance2after = await tokenContract.balanceOf(address2)
+            const balance3after = await tokenContract.balanceOf(address3)
+            //expect
+            expect(balance1after).to.be.above(balance1Before);
+            expect(balance2after).to.be.above(balance2Before);
+            expect(balance3after).to.be.above(balance3Before);
+            //segundo ciclo
+            //reset holders
+            distributor.resetHolders()
+            //cambiar airdrop tokens
+            const newAirdrop = "0x420FcA0121DC28039145009570975747295f2329"
+            await distributor.registerAirdropToken(newAirdrop)
+            //ingresar los adresses
+            await distributor.registerHolders(direcciones);
+            //transferir tokens al contrato
+            const coqContract = await ethers.getContractAt("IERC20", newAirdrop);
+            let addressImpersonatorCoq = "0xA4B237F14627EfD03394d66C3C1bE3878e07AB13";
+            await network.provider.request({
+                method: "hardhat_impersonateAccount",
+                params: [addressImpersonatorCoq]
+            });
+            const amountCoq = ethers.parseUnits("343401210",18)
+            const signer1 = await ethers.provider.getSigner(addressImpersonatorCoq)
+            await coqContract.connect(signer1).transfer(distributorAddress,amountCoq)
+            //balances airdrop antes
+            const balance1BeforeCoq = await coqContract.balanceOf(address1)
+            const balance2BeforeCoq = await coqContract.balanceOf(address2)
+            const balance3BeforeCoq = await coqContract.balanceOf(address3)
+            //llamar airdrop
+            await distributor.airdrop()
+            const balance1afterCoq = await coqContract.balanceOf(address1)
+            const balance2afterCoq = await coqContract.balanceOf(address2)
+            const balance3afterCoq = await coqContract.balanceOf(address3)
+            //expect
+            expect(balance1afterCoq).to.be.above(balance1BeforeCoq);
+            expect(balance2afterCoq).to.be.above(balance2BeforeCoq);
+            expect(balance3afterCoq).to.be.above(balance3BeforeCoq);
 
-    })
+
+                    })
+                })
+
 
 
 
